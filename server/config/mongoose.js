d71e7a6b06dfdd23d0f6817c430df5cd985cf14a
David Harris <david.harris@bramhalldata.com>
2014-12-14 18:35:43 -0500

    Creating new users

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-    crypto = require('crypto');
+    encrypt = require('../utilities/encryption');
 
 module.exports = function(config) {
     mongoose.connect(config.db);
@@ -21,7 +21,7 @@ module.exports = function(config) {
 
     userSchema.methods = {
         authenticate: function(passwordToMatch) {
-            return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+            return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
         }
 
     };
@@ -31,24 +31,16 @@ module.exports = function(config) {
     User.find({}).exec(function(err, collection) {
         if(collection.length === 0){
             var salt, hash;
-            salt = createSalt();
-            hash = hashPwd(salt, 'dave');
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'dave');
             User.create({firstName:'David', lastName:'Harris', username:'dave', salt:salt, hashed_pwd:hash, roles:['admin']});
-            salt = createSalt();
-            hash = hashPwd(salt, 'john');
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'john');
             User.create({firstName:'John', lastName:'Papa', username:'john', salt:salt, hashed_pwd:hash, roles:[]});
-            salt = createSalt();
-            hash = hashPwd(salt, 'dan');
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'dan');
             User.create({firstName:'Dan', lastName:'Wahlin', username:'dan', salt:salt, hashed_pwd:hash});
         }
     });
 };
 
-function createSalt() {
-    return crypto.randomBytes(128).toString('base64');
-}
-
-function hashPwd(salt, pwd) {
-    var hmac = crypto.createHmac('sha1', salt);
-    return hmac.update(pwd).digest('hex');
-}
